6efb4dea43f26da35c725ef72558c303a23e97ce
Craig P. Motlin <cmotlin@gmail.com>
2010-05-15 23:58:19 +0800

    Renamed VictoryPoints to VictoryCard.

--- src/main/scala/com/motlin/dominion/card/vp/Duchy.scala
+++ src/main/scala/com/motlin/dominion/card/vp/Duchy.scala
@@ -1,8 +1,8 @@
 package com.motlin.dominion.card.vp
 
-import com.motlin.dominion.card.VictoryPoints
+import com.motlin.dominion.card.VictoryCard
 
-case object Duchy extends VictoryPoints
+case object Duchy extends VictoryCard
 {
 	val cost = 5
 	val victoryPoints = 3
--- src/main/scala/com/motlin/dominion/card/vp/Estate.scala
+++ src/main/scala/com/motlin/dominion/card/vp/Estate.scala
@@ -1,8 +1,8 @@
 package com.motlin.dominion.card.vp
 
-import com.motlin.dominion.card.VictoryPoints
+import com.motlin.dominion.card.VictoryCard
 
-case object Estate extends VictoryPoints
+case object Estate extends VictoryCard
 {
 	val cost = 2
 	val victoryPoints = 1
