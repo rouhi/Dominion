9d467dcf01d9beecbc7b4f2d830cf1caaebd7130
Craig P. Motlin <cmotlin@gmail.com>
2010-05-16 00:07:05 +0800

    Refactored Supply to use more brief forms.

--- src/main/scala/com/motlin/dominion/Supply.scala
+++ src/main/scala/com/motlin/dominion/Supply.scala
@@ -1,27 +1,16 @@
 package com.motlin.dominion
 
 import card.Card
-import card.vp.{Province, Duchy, Estate}
-import collection.mutable.ListBuffer
+import card.vp._
 import collection.mutable.Map
 
 class Supply(numPlayers: Int)
 {
-	private val map: Map[Card, List[Card]] = Map()
-	map += (Estate -> cardList(vpCount, Estate))
-	map += (Duchy -> cardList(vpCount, Duchy))
-	map += (Province -> cardList(vpCount, Province))
+	private val vpCount = if (numPlayers > 2) 12 else 8
+	private val map = Map[Card, List[Card]]()
+	map(Estate) = List.fill(vpCount) {Estate}
+	map(Duchy) = List.fill(vpCount) {Duchy}
+	map(Province) = List.fill(vpCount) {Province}
 
-
-	private def cardList(count: Int, card: Card): List[Card] =
-	{
-		val buffer = new ListBuffer[Card]
-	 	for(i <- 1 to count) buffer += card
-		buffer.toList
-	}
-
-	private def vpCount: Int = { if (numPlayers > 2) 12 else 8 }
-
-	def count(card: Card): Int = { map(card).length }
-
+	def count(card: Card): Int = map(card).length
 }
