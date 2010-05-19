af4b5fac8f053495026b5fda59981ff7680d83e0
Craig P. Motlin <cmotlin@gmail.com>
2010-05-19 01:11:50 -0400

    Create build configuration for simple build tool.

---
+++ project/build/Build.scala
@@ -0,0 +1,12 @@
+import sbt._
+
+class Build(info: ProjectInfo) extends DefaultProject(info)
+{
+	val scalaToolsSnapshots = ScalaToolsSnapshots
+
+	val junit = "junit" % "junit" % "4.8.1"
+	val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT"
+	val junitInterface = "com.novocode" % "junit-interface" % "0.3"
+
+	override def testFrameworks = super.testFrameworks ++ List(new TestFramework("com.novocode.junit.JUnitFrameworkNoMarker"))
+}
