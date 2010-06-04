27c203a83fe3f7169c999625afcb6441e62f4640
Craig <Craig@Wayne.(none)>
2010-06-03 23:26:39 -0400

    Upgrade to Scala 2.8.0 RC3.

--- project/build/Build.scala
+++ project/build/Build.scala
@@ -5,7 +5,7 @@ class Build(info: ProjectInfo) extends DefaultProject(info)
 	val scalaToolsSnapshots = ScalaToolsSnapshots
 
 	val junit = "junit" % "junit" % "4.8.1"
-	val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT"
+	val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT"
 	val junitInterface = "com.novocode" % "junit-interface" % "0.3"
 
 	override def testFrameworks = super.testFrameworks ++ List(new TestFramework("com.novocode.junit.JUnitFrameworkNoMarker"))
