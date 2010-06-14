f57c62c9b762b0c39a90a1f752dcf83339c2b877
Craig P. Motlin <cmotlin@gmail.com>
2010-06-14 00:20:21 -0400

    Move to Scala 2.8.0-RC5. Updated other libraries.

--- project/build/Build.scala
+++ project/build/Build.scala
@@ -2,17 +2,15 @@ import sbt._
 
 class Build(info: ProjectInfo) extends DefaultProject(info)
 {
-	val slf4jVersion = "1.6.0"
-
 	val scalaToolsSnapshots = ScalaToolsSnapshots
 
-	val guice = "com.google.inject" % "guice" % "2.0"
-	val slf4j_api = "org.slf4j" % "slf4j-api" % slf4jVersion
-	val slf4j_log4j12 = "org.slf4j" % "slf4j-log4j12" % slf4jVersion
+	val guice = "com.google.inject" % "guice" % "latest.release"
+	val slf4j_api = "org.slf4j" % "slf4j-api" % "latest.release"
+	val slf4j_log4j12 = "org.slf4j" % "slf4j-log4j12" % "latest.release" % "runtime"
 
-	val junit = "junit" % "junit" % "4.8.1"
-	val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT"
-	val junitInterface = "com.novocode" % "junit-interface" % "0.3"
+	val junit = "junit" % "junit" % "latest.release" % "test"
+	val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC5-SNAPSHOT"
+	val junitInterface = "com.novocode" % "junit-interface" % "latest.release" % "test"
 
 	override def testFrameworks = super.testFrameworks ++ List(new TestFramework("com.novocode.junit.JUnitFrameworkNoMarker"))
 }
